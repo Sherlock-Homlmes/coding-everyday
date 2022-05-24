26b57b5983872fe9b0646b0baeff41308cf9918a
Sherlock-Homlmes <dbsiksfikf@gmail.com>
2022-05-24 10:04:48 +0700

    Add files via upload

--- backend/python/fastapi/betterme.news/database/mongodb/discord_user_database.py
+++ backend/python/fastapi/betterme.news/database/mongodb/discord_user_database.py
@@ -1,9 +1,11 @@
 from threading import Thread
 import os
 import time
+import asyncio
 ######database
 #
 from pymongo import MongoClient
+import numpy as np
 
 from dotenv import load_dotenv
 import json,io
@@ -29,7 +31,7 @@ def discord_user(current_user):
     print("new user")
 
     user_info = {
-    "id": current_user.id,
+    "user_id": current_user.id,
     "email": current_user.email,
 
     "is_verified": current_user.is_verified,
@@ -43,8 +45,8 @@ def discord_user(current_user):
     user_db.insert_one(user_info)
 
 #take_data
-def take_discord_user_by_id(value):
-  element = user_db.find( { 'id': value } )
+def take_discord_user_by_id(value) -> list:
+  element = user_db.find( { 'user_id': value } ).limit(1)
   value = []
   for ele in element:
     value.append(ele)
@@ -53,6 +55,16 @@ def take_discord_user_by_id(value):
     return None
   else:
     return value[0]
+
+async def take_discord_user_by_id_list(list_value) -> dict:
+  element = user_db.find( { 'user_id': {'$in':list_value} } )
+  value = {}
+  for ele in element:
+    value[str(ele['user_id'])] = ele
+  
+  #print(f'List value:{list_value}')
+  #print(f'Value:{value}')
+  return value
 
 #update
 def update_discord_user_by_id(current_user):
